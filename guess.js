3691ed93135a66004b93a28138fd44931a7cd5e6
camilleharmon <camille.harmon@mullenmail.com>
2022-11-17 14:36:25 -0700

    Work1

--- guess.js
+++ guess.js
@@ -1,16 +1,21 @@
-
-    function guessNumber(){
-
-        let secretNum = Math.floor(Math.random() * 5) + 1;
+let secretNum = Math.floor(Math.random() * 5) + 1;
+    
+function guessNumber(){
+        
         /* document.getElementById("Word").setAttribute("style", "color: white") */
+        var guess = document.getElementById("guess").value;
+        
         if(guess > secretNum){
 
             alert("Too HIGH");
+            alert("Number: " + secretNum);
         }else if (guess < secretNum){
 
             alert("Too LOW");
+            alert("Number: " + secretNum);
         }else{
 
             alert("You got it!");
+            alert("Number: " + secretNum);
         }
     }
